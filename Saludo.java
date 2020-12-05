c5d3f8d690a2939428be91ced4700cd2e738ce5e
leopo <leopo@DESKTOP-IFBRQ8T>
2020-12-05 20:06:31 +0100

    Complementando saludo

--- Saludo.java
+++ Saludo.java
@@ -2,8 +2,8 @@
 public class Saludo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Proyecto tontito para probar GIT
+		System.out.println("Hola developpers");
 	}
 
 }
